52dfa0157f2aed18ab1c1bde656a7ee11f56650a
iTeckHD <ituankha.tran+github@gmail.com>
2020-03-12 10:54:12 +0100

    feat(api): toggle

--- packages/api/src/light.controller.ts
+++ packages/api/src/light.controller.ts
@@ -48,3 +48,23 @@ lightController.post("api/lighs/:light/_turnOff", ctx => {
     ctx.body = err;
   }
 });
+
+lightController.post("api/lighs/:light/_toggle", ctx => {
+  try {
+    if (isNaN(ctx.params.light)) {
+      throw new Error("Light is not a number");
+    }
+
+    if (getState(ctx.params.light)) {
+      turnOff(parseInt(ctx.params.light, 10));
+    } else {
+      turnOn(parseInt(ctx.params.light, 10));
+    }
+
+    ctx.status = 200;
+    ctx.body = null;
+  } catch (err) {
+    ctx.status = 500;
+    ctx.body = err;
+  }
+});
